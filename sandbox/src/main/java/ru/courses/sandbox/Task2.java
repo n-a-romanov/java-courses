66fb7e9b8309b78e1501c4534c50501498799bc6
HOME-PC\nikita.romanov <nikita.romanov@quadcode.com>
2021-11-27 20:56:23 +0300

    Задание 2 (пункт 4) Укорочена формула

--- sandbox/src/main/java/ru/courses/sandbox/Task2.java
+++ sandbox/src/main/java/ru/courses/sandbox/Task2.java
@@ -7,11 +7,8 @@ public class Task2 {
         Point p1 = new Point(1,1);
         Point p2 = new Point(4,5);
 
-        System.out.println("Расстояние между точкой p1("+p1.x+","+p1.y+") и точкой p2("+p2.x+","+p2.y+") равно "+distance(p1,p2));
+        System.out.println("Расстояние между точкой p1("+p1.x+","+p1.y+") и точкой p2("+p2.x+","+p2.y+") равно "+p1.distance(p2));
         }
 
-    public static double distance(Point p1, Point p2) {
-        return Math.sqrt((p2.x-p1.x)*(p2.x-p1.x) + (p2.y-p1.y)*(p2.y-p1.y));
-    }
 }
 
