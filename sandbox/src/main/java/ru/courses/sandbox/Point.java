ac4e8dae721e2501a5429a226ccfe0d3257522c9
HOME-PC\nikita.romanov <nikita.romanov@quadcode.com>
2021-11-27 20:34:55 +0300

    Добавил конструктор в класс Point

--- sandbox/src/main/java/ru/courses/sandbox/Point.java
+++ sandbox/src/main/java/ru/courses/sandbox/Point.java
@@ -3,4 +3,8 @@ package ru.courses.sandbox;
 public class Point {
     public double x;
     public double y;
+    public Point (double x, double y){
+        this.x=x;
+        this.y=y;
+    }
 }
--- sandbox/src/main/java/ru/courses/sandbox/Task2.java
+++ sandbox/src/main/java/ru/courses/sandbox/Task2.java
@@ -4,12 +4,8 @@ public class Task2 {
 
 
     public static void main (String[] args) {
-        Point p1 = new Point();
-        Point p2 = new Point();
-        p1.x=1;
-        p1.y=1;
-        p2.x=4;
-        p2.y=5;
+        Point p1 = new Point(1,1);
+        Point p2 = new Point(4,5);
 
         System.out.println("Расстояние между точкой p1("+p1.x+","+p1.y+") и точкой p2("+p2.x+","+p2.y+") равно "+distance(p1,p2));
         }
