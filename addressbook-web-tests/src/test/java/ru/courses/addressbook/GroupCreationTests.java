947ad9d2c181c1575206cc5a346cf17ef20e7d00
HOME-PC\nikita.romanov <nikita.romanov@quadcode.com>
2021-11-28 17:13:34 +0300

    Выделены вспомогательные методы. Авторизация перенесена @BeforeMethod

--- addressbook-web-tests/src/test/java/ru/courses/addressbook/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/courses/addressbook/GroupCreationTests.java
@@ -15,10 +15,6 @@ public class GroupCreationTests {
     wd = new ChromeDriver();
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     js = (JavascriptExecutor) wd;
-  }
-
-  @Test
-  public void testGroupCreation() throws Exception {
     wd.get("http://localhost/addressbook/");
     wd.findElement(By.name("user")).click();
     wd.findElement(By.name("user")).clear();
@@ -27,6 +23,10 @@ public class GroupCreationTests {
     wd.findElement(By.name("pass")).clear();
     wd.findElement(By.name("pass")).sendKeys("secret");
     wd.findElement(By.xpath("//input[@value='Login']")).click();
+  }
+
+  @Test
+  public void testGroupCreation() throws Exception {
     wd.findElement(By.linkText("groups")).click();
     wd.findElement(By.name("new")).click();
     wd.findElement(By.name("group_name")).click();
