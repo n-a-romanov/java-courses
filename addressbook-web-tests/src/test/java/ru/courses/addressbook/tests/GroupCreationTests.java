35ae531c291ef2cde63bd575492df4a90559a99d
HOME-PC\nikita.romanov <nikita.romanov@quadcode.com>
2022-01-09 20:37:57 +0300

    Включение логирования

--- addressbook-web-tests/src/test/java/ru/courses/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/courses/addressbook/tests/GroupCreationTests.java
@@ -3,6 +3,8 @@ package ru.courses.addressbook.tests;
 import com.google.common.reflect.TypeToken;
 import com.google.gson.Gson;
 import com.thoughtworks.xstream.XStream;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.courses.addressbook.model.GroupData;
@@ -20,6 +22,8 @@ import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreationTests extends TestBase {
+
+    Logger logger = LoggerFactory.getLogger(GroupCreationTests.class);
 
     @DataProvider
     public Iterator<Object[]> validGroupsFromXml() throws IOException {
@@ -62,6 +66,7 @@ public class GroupCreationTests extends TestBase {
         Groups after = app.group().all();
         assertThat(after, equalTo(
             before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
+
     }
 
     @Test
