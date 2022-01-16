74eb28419e5e3b4d4b4a29ca16b63b3e04f0d2be
HOME-PC\nikita.romanov <nikita.romanov@quadcode.com>
2022-01-16 23:43:55 +0300

    Подготовлена основа для тестов для mantis

---
+++ mantis-tests/src/test/java/ru/courses/mantis/tests/TestBase.java
@@ -0,0 +1,21 @@
+package ru.courses.mantis.tests;
+
+import org.openqa.selenium.remote.BrowserType;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import ru.courses.mantis.appmanager.ApplicationManager;
+
+public class TestBase {
+
+        public static final ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
+
+    @BeforeSuite(alwaysRun = true)
+    public void setUp() throws Exception {
+        app.init();
+    }
+
+    @AfterSuite(alwaysRun = true)
+    public void tearDown() throws Exception {
+    }
+
+}
