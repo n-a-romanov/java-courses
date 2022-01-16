b63a43bf932d94b3c07d1054f9d3b05865b6199c
HOME-PC\nikita.romanov <nikita.romanov@quadcode.com>
2022-01-17 01:29:20 +0300

    Добавлена ленивая реализация запуска браузера. Добавлены тесты на логин и загатовка на регистрацию.

--- mantis-tests/src/test/java/ru/courses/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/courses/mantis/appmanager/ApplicationManager.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
+import org.testng.junit.JUnit4TestRunner;
 
 import java.io.File;
 import java.io.FileReader;
@@ -16,12 +17,13 @@ import java.util.concurrent.TimeUnit;
 public class ApplicationManager {
 
     private final Properties properties;
-    WebDriver wd;
+    private WebDriver wd;
 
     private JavascriptExecutor js;
     private String browser;
+    private RegistrationHelper registrationHelper;
 
-        public ApplicationManager(String browser) {
+    public ApplicationManager(String browser) {
         this.browser = browser;
         properties = new Properties();
     }
@@ -29,16 +31,42 @@ public class ApplicationManager {
     public void init() throws IOException {
         String target = System.getProperty("target", "local");
         properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
+    }
 
-        if (browser.equals(BrowserType.CHROME)) {
-            wd = new ChromeDriver();
-        } else if (browser.equals(BrowserType.FIREFOX)){
-            wd = new FirefoxDriver();
-        } else if (browser.equals(BrowserType.IE)){
-            wd = new InternetExplorerDriver();
+    public void stop() {
+            if (wd !=null) {
+                wd.quit();
+            }
+    }
+
+    public HttpSession newSession() {
+          return new HttpSession(this);
+    }
+
+    public String getProperty(String key) {
+            return properties.getProperty(key);
+    }
+
+    public RegistrationHelper registration() {
+        if (registrationHelper == null) {
+            registrationHelper = new RegistrationHelper(this);
         }
-        wd.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
-        js = (JavascriptExecutor) wd;
-        wd.get(properties.getProperty("web.baseUrl"));
+        return registrationHelper;
+    }
+
+    public WebDriver getDriver() {
+            if (wd == null) {
+                if (browser.equals(BrowserType.CHROME)) {
+                    wd = new ChromeDriver();
+                } else if (browser.equals(BrowserType.FIREFOX)){
+                    wd = new FirefoxDriver();
+                } else if (browser.equals(BrowserType.IE)){
+                    wd = new InternetExplorerDriver();
+                }
+                wd.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
+                js = (JavascriptExecutor) wd;
+                wd.get(properties.getProperty("web.baseUrl"));
+            }
+            return wd;
     }
 }
