67d6fd3b71c1f1c17af39f2549fed001d6efa800
HOME-PC\nikita.romanov <nikita.romanov@quadcode.com>
2022-01-26 02:17:33 +0300

    Реализована возможность указывать платформу

--- addressbook-web-tests/src/test/java/ru/courses/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/courses/addressbook/appmanager/ApplicationManager.java
@@ -1,6 +1,7 @@
 package ru.courses.addressbook.appmanager;
 
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -52,6 +53,7 @@ public class ApplicationManager {
         } else {
             DesiredCapabilities capabilities = new DesiredCapabilities();
             capabilities.setBrowserName(browser);
+            capabilities.setPlatform(Platform.fromString(System.getProperty("planform","win10")));
             wd = new RemoteWebDriver (new URL(properties.getProperty("selenium.server")), capabilities);
         }
         wd.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
